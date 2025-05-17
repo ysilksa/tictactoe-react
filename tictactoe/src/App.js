64a5656eaa28e8369842a4f7e5faf5aeb8ee206c
ysilksa <isabella.yan.business@gmail.com>
2025-05-16 23:12:04 -0500

    add click handler to board

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,23 +1,34 @@
 import './App.css';
+import { useState } from 'react';
 import Square from './components/Shape.jsx';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  // define handleClick function in board
+  function handleClick(curr) {
+    const nextSquares = squares.slice(); 
+    nextSquares[curr] = "X";
+    setSquares(nextSquares);
+  }
+  // => is arrow function
+
   return (
     <div className="App">
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[0]} clickHandler={() => handleClick(0)}/>
+        <Square value={squares[1]} clickHandler={() => handleClick(1)}/>
+        <Square value={squares[2]} clickHandler={() => handleClick(2)}/>
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[3]} clickHandler={() => handleClick(3)}/>
+        <Square value={squares[4]} clickHandler={() => handleClick(4)}/>
+        <Square value={squares[5]} clickHandler={() => handleClick(5)}/>
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square num={9}/>
+        <Square value={squares[6]} clickHandler={() => handleClick(6)}/>
+        <Square value={squares[7]} clickHandler={() => handleClick(7)}/>
+        <Square value={squares[8]} clickHandler={() => handleClick(8)}/>
       </div> 
     </div>
   );
--- tictactoe/src/components/Shape.jsx
+++ tictactoe/src/components/Shape.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-export default function Square() {
+export default function Square({value}, {clickHandler}) {
     // use useState to handle value of square 
-    const [value, setValue] = useState(null);
+    // const [value, setValue] = useState(null);
 
     // add handleClick object to Square
-    function handleClick() {
-        console.log('clicked!');
-        setValue('X');
-    }
+    // function handleClick() {
+    //     console.log('clicked!');
+    //     setValue('X');
+    // }
 
     return (
     <button 
         className="square"
-        onClick={handleClick} // here, we add handleClick to the button object 
+        onClick={clickHandler} // here, we add handleClick to the button object 
     >
         {value}
     </button>
