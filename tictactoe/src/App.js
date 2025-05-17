8318b7c338eb800a4784309f585bdcc9f9b30f0f
ysilksa <isabella.yan.business@gmail.com>
2025-05-16 21:56:23 -0500

    add initial files

---
+++ tictactoe/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Square from './components/Shape.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Square/>
+      <Square/>
+      <Square/>
+    </div>
+  );
+}
+
+export default App;
